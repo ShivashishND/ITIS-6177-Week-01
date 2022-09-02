a10d8ce674691778f9bb2342f8150a18858031bb
Shivashish Naramdeo <snaramde@uncc.edu>
2022-09-02 15:51:58 -0400

    created node server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World from Shivashish')
+var http = require('http')
+
+const port = 3000
+
+const app = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end('Hello World from Shivashish')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`)
+})
